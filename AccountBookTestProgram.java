697acd1b104942cde1c3f33ba0ed83911b8feab7
InJu Yoon <dlswn94@naver.com>
2017-12-03 18:29:49 +0900

    AccountBookTestProgram interface

---
+++ AccountBookTestProgram.java
@@ -0,0 +1,53 @@
+class AccountBook {
+
+	public void actAccountBook() {
+		// AccountBook을 반복적으로 수행할 수 있도록 하는 메소드
+	}
+
+	public void showMenu() {
+		// 메뉴를 보여주는 메소드
+	}
+
+	public void actMenu() {
+		// 각각의 메뉴들을 실행하는 메소드
+	}
+
+	public void showList() {
+		// 가계부 기록을 보여주는 메소드
+	}
+
+	public void addList() {
+		// 가계부 기록을 추가하는 메소드
+	}
+
+	public void supplementBudget(int nowindexnum) {
+		// 현재 인덱스 번호를 인수로 받아 예산에 더해주는 메소드
+		// 수입
+	}
+
+	public void subtractBudget(int nowindexnum) {
+		// 현재 인덱스 번호를 인수로 받아 예산에서 빼주는 메소드
+		// 지출
+	}
+
+	public void modifyMember() {
+		// 가계부를 수정하는 메소드
+	}
+
+	public void deleteMember() {
+		// 가계부 기록을 삭제하는 메소드
+	}
+
+	public void backMain() {
+		// 메인 메뉴로 돌아가는 메소드
+	}
+
+}
+
+public class AccountBookTestProgram {
+
+	public static void main(String[] args) {
+		// AccountBook 클래스의 actAccountBook 메소드를 수행
+	}
+
+}
