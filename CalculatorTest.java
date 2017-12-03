05975265b3e3ef40ed7306dfc1a867592d21f50d
Sumin <sumincathy@naver.com>
2017-12-03 18:32:38 +0900

    add calculator interface

---
+++ CalculatorTest.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class CalculatorTest {
+	public static void main(String[] args) {
+
+		
+	}
+}
+
+class Calculator {
+
+	public void printMenu() {
+		
+	}
+
+	public void calculateTwoNumbers() {
+
+	}
+
+	public void convertUnits() {
+	
+	}
+
+	public void convertBetweenPoundAndKg() { 
+
+	}
+
+	public void convertBetweenInchAndCm() {
+		
+	}
+
+	public void convertBetweenFahrenheitAndCelsius() {
+		
+	}
+
+	private double fToC(double input) {
+		return input;
+	}
+
+	private double CToF(double input) {
+		return input;
+	}
+}
+
